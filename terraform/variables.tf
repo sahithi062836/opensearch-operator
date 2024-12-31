792c30efc6cf60f0cc878b46b65a0e7261b4bb59
sahithi062836 <sahithidulam36@gmail.com>
2024-12-31 14:57:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,3 +22,19 @@ variable "node_vm_size" {
   description = "VM size for AKS nodes"
   default     = "Standard_DS2_v2"
 }
+
+# New variables for autoscaling
+variable "enable_autoscaler" {
+  description = "Enable or disable AKS cluster autoscaler"
+  default     = true
+}
+
+variable "min_node_count" {
+  description = "Minimum number of nodes in the AKS cluster (for autoscaling)"
+  default     = 1
+}
+
+variable "max_node_count" {
+  description = "Maximum number of nodes in the AKS cluster (for autoscaling)"
+  default     = 5
+}
